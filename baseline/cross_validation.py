cef3898ff338c8679b07f0cfcf91074f38a53875
Timothy Zhang <tsme4wk@gmail.com>
2019-07-02 13:13:59 -0700

    Cross validation done.
    Squashed commit of the following:
    
    commit ef4f7b2a492f4099fd72ad87d86799f8e152f48c
    Author: Timothy Zhang <tsme4wk@gmail.com>
    Date:   Tue Jul 2 13:07:18 2019 -0700
    
        added function description & updated naming
    
    commit 13e4a28fd9778047ec9595ea70301fa5254cddb3
    Author: Timothy Zhang <tsme4wk@gmail.com>
    Date:   Tue Jul 2 12:44:34 2019 -0700
    
        added option for shuffling and seed
    
    commit 5a25fab3bc51dbcac1cd174ba4c33e0cb3bd8008
    Author: Timothy Zhang <tsme4wk@gmail.com>
    Date:   Mon Jul 1 16:17:42 2019 -0700
    
        simply choosing first 10 games done
    
    commit 3a83d475c36021cf471a1f904fb5e065be16aa15
    Author: Timothy Zhang <tsme4wk@gmail.com>
    Date:   Mon Jul 1 16:05:51 2019 -0700
    
        X & Y done
    
    commit a5be23cd612ce89a6c72659bffdd0719dc7fec2e
    Author: Timothy Zhang <tsme4wk@gmail.com>
    Date:   Sun Jun 30 22:38:38 2019 -0700
    
         X,Y allocation done
    
    commit 7dc573ef779e25565510abf67d09007027c45b07
    Author: Timothy Zhang <tsme4wk@gmail.com>
    Date:   Sun Jun 30 22:16:58 2019 -0700
    
        added new arguments and returns && binary to int function
    
    commit 4f71d5b3c1657aa3c6b1c1afc4aa64a8c4332e0f
    Author: Timothy Zhang <tsme4wk@gmail.com>
    Date:   Sun Jun 30 21:50:43 2019 -0700
    
        added doc of input & output

---
+++ baseline/cross_validation.py
@@ -0,0 +1,92 @@
+import pickle
+import numpy as np
+import random
+
+# TODO:
+#   - Add option for choosing int/binary/one-hot; currently just binary
+#     Problem: one-hot or int will be too large b/c it's s 658 bit integer.
+
+SIZE_OBS_VEC = 658
+SIZE_ACT_VEC = 20
+
+def CV(pkl_path='./data/Hanabi-Full_2_6_150.pkl', agent='rainbow_agent_6',
+       type_obs='binary', shuffle=False, seed=1234):
+    """ Generate training sets with a size of 10 games and validation sets
+        containing the remaining games.
+
+    Arguments:
+        - pkl_path: str, default './data/Hanabi-Full_2_6_150.pkl'
+            Path to the pickle file that contains the output generated from
+            create_data.py. First 10 games are chosen for training, and the
+            rest will be for validation.
+        - agent: str, default 'rainbow_agent_6'
+            Name of the agent to use.
+        - type_obs: str, default binary
+            Type of the observations. Must be in {'binary', 'int'}.
+        - shuffle: boolean, default false
+            If true, 10 games are randomly chosen instead of simply choosing
+            the first 10.
+        - seed: int, default 1234
+            Seed for shuffling. Use None to set current time as seed.
+
+    Returns:
+        - X_tr: np.matrix
+            Training atrix that contains the observations in following format:
+            [[observatoin of round 1 in game 1],
+             [observatoin of round 2 in game 1],
+             ...
+             [observatoin of round 1 in game 2],
+             ...
+             [observatoin of round N in game 10]]
+        - Y_tr: np.matrix
+            Training matrix that contains the actions in following format:
+            [[action of round 1 in game 1],
+             [action of round 2 in game 1],
+             ...
+             [action of round 1 in game 2],
+             ...
+             [action of round N in game 10]]
+        - X_va: np.matrix
+            Validation matrix that contains the observations.
+        - Y_va: np.matrix
+            Validation matrix that contains the actions.
+    """
+
+    def bin2int(bin_list):
+        """ Converts an binary integer list into an integer.
+            Ex: [0,1,0] -> 2
+        """
+        return int("".join(str(x) for x in bin_list), 2)
+
+    with open(pkl_path, 'rb') as f:
+        raw = pickle.load(f)
+
+    lst = raw[agent] # list of info for the chosen agent
+    ind = list(range(len(lst))) # game indices
+
+    if shuffle:
+        random.seed(seed)
+        random.shuffle(ind)
+
+    # Determine the size of the matrices
+    n_rows = 0
+    for n, i in enumerate(ind): # [n]umber of games gone thru & [i]dx of game
+        n_rnds =  len(lst[i][0])
+        n_rows += n_rnds
+        # Save the row num for the training set cutoff
+        if n == 9:
+            n_tr = n_rows
+
+    X = np.zeros([n_rows, SIZE_OBS_VEC])
+    Y = np.zeros([n_rows, SIZE_ACT_VEC])
+
+    cur_idx = 0
+    for n, i in enumerate(ind):
+        obs = np.matrix(lst[i][0])
+        act = np.matrix(lst[i][1])
+        X[cur_idx:(cur_idx + obs.shape[0]), :] = obs
+        Y[cur_idx:(cur_idx + act.shape[0]), :] = act
+        cur_idx += act.shape[0]
+        assert(obs.shape[0] == act.shape[0])
+
+    return X[:n_tr, :], Y[:n_tr, :], X[n_tr:, :], Y[n_tr:, :]
