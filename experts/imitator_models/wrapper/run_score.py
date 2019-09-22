fc7d6ddf5a15c5839987d5787104ca174ccc52eb
Timothy Zhang <tsme4wk@gmail.com>
2019-09-22 15:23:09 -0700

    fixed score always being zero

--- experts/imitator_models/wrapper/run_score.py
+++ experts/imitator_models/wrapper/run_score.py
@@ -1,26 +1,25 @@
 import csv
 import os, sys
 import numpy as np
+import glob
 
 from create_agent_data import *
 
-def run_game():
-    agents_0 = ['ex.h5','ex.h5']
-    agents_1 = ['ex.h5','ex.h5']
-    num_games = 10
+def run_game(n=50, pattern='best_models/*.h5'):
+    agents = glob.glob(pattern)
+    agents.sort()
+    num_games = n
     full_score = 25
     score_matrix = []
-    percent_matrix = []
-    for a0 in agents_0:
+    avg_score = []
+    for a0 in agents:
         scores_list = []
-        percent_list = []
-        for a1 in agents_1:
+        avg_list = []
+        for a1 in agents:
             scores = DataCreator(num_games, a0, a1).create_data()
-            percents = [ s / full_score for s in scores]
             scores_list.append(scores)
-            percent_list.append(percents)
+            avg_list.append(sum(scores) * 1.0 / n)
         score_matrix.append(scores_list)
-        percent_matrix.append(percents)
+        avg_score.append(avg_list)
 
-if __name__ == '__main__':
-    run_game()
+    return score_matrix, avg_score, agents
